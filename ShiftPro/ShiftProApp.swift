9366e843feb6b1c0e32349d7061033e4864c3c18
doris <doris070714@gmail.com>
2025-07-08 19:07:40 +0800

    Initial Commit

---
+++ ShiftPro/ShiftProApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShiftProApp.swift
+//  ShiftPro
+//
+//  Created by Doris Wen on 2025/7/8.
+//
+
+import SwiftUI
+
+@main
+struct ShiftProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
